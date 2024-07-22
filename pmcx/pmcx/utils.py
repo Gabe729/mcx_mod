5ffa99bc83b4e991f3c3fb6747ce6f8530679035
Gabe729 <oyarzo0002@gmail.com>
2024-07-22 11:30:14 +1000

    Implement handling of missing 'jonesprop' in Python cfg

--- pmcx/pmcx/utils.py
+++ pmcx/pmcx/utils.py
@@ -536,6 +536,10 @@ def mcxlab(*args):
             ) and "unitinmm" in args[0]:
                 args[0]["vol"] = args[0]["vol"] * args[0]["unitinmm"]
 
+        # Handle missing jonesprop
+        if 'jonesprop' not in args[0]:
+            args[0]['jonesprop'] = None
+
         if "tstart" not in args[0]:
             args[0]["tstart"] = 0
 
